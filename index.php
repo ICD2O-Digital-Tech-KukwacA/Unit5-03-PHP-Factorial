ee8cda659d44a0685bca454526ada5718bc1e372
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-06-16 12:16:00 +0000

    Unit5-03 PHP

--- index.php
+++ index.php
@@ -12,25 +12,20 @@
     <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
     <link rel="manifest" href="./Favicons/site.webmanifest">
-    <!-- Link to CSS file -->
-    <link rel="stylesheet" href="./css/style.css">
     <title>Factorial, Do While Loops</title>
   </head>
   <body>
     
-    <!-- form to get the indexes of refraction from the user -->
     <form action="./results.php" method="post" target="results">
-      <label for="firstNumber">First Number:</label>
-      <input type="float" id="firstNumber" name="firstNumber"><br><br>
-      <label for="secondNumber">Second Number:</label>
-      <input type="float" id="secondNumber" name="secondNumber"><br><br>
-      <input type="submit" value="Display Result">
+      <label for="userNumber">User Number:</label>
+      <input type="integer" id="userNumber" name="userNumber"><br><br>
+      <input type="submit" value="Display Factorial">
     </form>
 
     <!-- iframe for the results to show on the web page. -->
     <iframe id="results" name="results">
       <div id="user-info">
-        <div id="$numbers"></div>
+        <div id="$number"></div>
       </div>
     </iframe>  
   </body>
--- results.php
+++ results.php
@@ -1,22 +1,21 @@
 <?php
     // initalize numbers as an empty string
-    $sum = 0;
-    $numbers = "";
+    $result = 1;
+    $counter = 1;
 
     // Setting the variables and validating input
-    $userNumFirst = $_POST['firstNumber'];
-    $userNumSecond = $_POST['secondNumber'];
+    $userNum = $_POST['userNumber'];
+    if (!is_numeric($userNum) || intval($userNum) != $userNum || $userNum < 0) {
+        echo "Please enter a non-negative integer.";
+        exit;
+    }
+    $userNum = intval($userNum);
 
-for ($counter = 0; $counter < $userNumSecond; $counter++) {
-    if ($counter < $userNumSecond - 1){
-        $numbers = $numbers . $userNumFirst . "+";
-    }
-    else {
-        $numbers = $numbers . $userNumFirst;
-    }
-    $sum = $sum + $userNumFirst;
-}
-?>	
+    do {
+        $result = $result * $counter;
+    } while ($counter <= $userNum);
+
+?>
 	<!-- display the results -->
     <h4>Results:</h4>
-    <?php echo $numbers . " = " . $sum ?>
+    <?php echo $result ?>
